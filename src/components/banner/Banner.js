d21557bcd776be4f5321d653d803e42c6794401c
Zachary Martin <zachsm@stanford.edu>
2024-12-03 20:10:20 -0800

    added res folder with images folder and a linkedin image file

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Banner.module.css";
-
+import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div>
-      <h1>My Portfolio</h1>
+      <h1 className={styles.text}>
+        Hey! Yo! I’m Zach Martin and I’m a visual
+        computing software engineer.
+      </h1>
+      
     </div>
   );
 };
