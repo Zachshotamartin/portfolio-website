b799d28ed4e58f8d14bb88927e3202f4606060b9
Zachary Martin <zachsm@stanford.edu>
2024-12-05 11:32:49 -0800

    adding some logos and links

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Main.module.css";
 import Headshot from "../headshot/Headshot";
 import githubLogo from "../../res/images/githubIcon.png";
+import linkedinLogo from "../../res/images/linkedinIcon.png";
 const Main = () => {
   return (
     <div className={styles.container}>
@@ -16,13 +17,22 @@ const Main = () => {
           with a focus in Computer Graphics. I love working with cool consumer
           apps and softwares that put people in awe.
         </p>
-        <a
-          href="https://github.com/Zachshotamartin"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubLogo} alt="Github" className={styles.icon} />
-        </a>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://github.com/Zachshotamartin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubLogo} alt="Github" className={styles.icon} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/zachary-martin-0a6437185"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedinLogo} alt="Linkedin" className={styles.icon} />
+          </a>
+        </div>
       </div>
     </div>
   );
