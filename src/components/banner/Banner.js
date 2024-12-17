3080e088fa69035e28146f12012e7881647f2a89
Zachary Martin <zachsm@stanford.edu>
2024-12-16 21:04:56 -0800

    fixed the deletion of some images and videos

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,27 +1,19 @@
 /*************  ✨ Codeium Command 🌟  *************/
 import React from "react";
 import styles from "./Banner.module.css";
-import linkedinLogo from "../../res/images/LinkedinLogo.avif";
+import linkedinLogo from "../../res/images/linkedinIcon3.png";
 import siteLogo from "../../res/images/sitelogo.png";
 const Banner = () => {
   return (
     <div className={styles.container}>
-      <img
-        src={siteLogo}
-        alt="Site Logo"
-        className={`${styles.image} ${styles.imageLeft}`}
-      />
+      <img src={siteLogo} alt="Site Logo" className={styles.imageLeft} />
       <h1 className={styles.text}>Hey! Yo! Visual Computation</h1>
       <a
         href="https://www.linkedin.com/in/zachary-martin-0a6437185/"
         target="_blank"
         rel="noreferrer"
       >
-        <img
-          src={linkedinLogo}
-          alt="LinkedIn"
-          className={`${styles.image} ${styles.imageRight}`}
-        />
+        <img src={linkedinLogo} alt="LinkedIn" className={styles.imageRight} />
       </a>
     </div>
   );
